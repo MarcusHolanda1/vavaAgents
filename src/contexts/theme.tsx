289d314210cddc3f6f639a6740acbfeaa51405af
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-08-10 10:01:25 -0300

    feat: creating provider to handle with Theme

---
+++ src/contexts/theme.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useCallback, useContext, useState } from "react";
+
+export type Themes = "dark" | "light";
+
+type ThemeContextData = {
+  currentTheme: Themes;
+  handleChangeTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextData>({} as ThemeContextData);
+
+export const ThemeProvider: React.FC = ({ children }) => {
+  const [selectedTheme, setSelectedTheme] = useState<Themes>("light");
+
+  const handleChangeTheme = useCallback(() => {
+    setSelectedTheme(selectedTheme === "light" ? "dark" : "light");
+  }, [selectedTheme]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        currentTheme: selectedTheme,
+        handleChangeTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export function useTheme(): ThemeContextData {
+  const context = useContext(ThemeContext);
+
+  return context;
+}
