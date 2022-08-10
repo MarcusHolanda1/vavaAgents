64cbf648b8e3f6c29891ecb024ed52fc933b7370
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-08-10 10:08:31 -0300

    feat: creating Home screen with theme

---
+++ src/UI/screens/Home/index.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { Switch, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { useTheme } from "../../../contexts/theme";
+import IMAGES from "../../../assets/";
+import * as S from "./styles";
+import theme from "../../theme";
+import { Page } from "../../structures";
+import { Button, Text } from "../../components";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../../../navigation";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+type HomeScreenProps = NativeStackNavigationProp<RootStackParamList, "Cards">;
+
+const Home = () => {
+  const { handleChangeTheme } = useTheme();
+  const { currentTheme } = useTheme();
+  const navigation = useNavigation<HomeScreenProps>();
+
+  const [switchIsEnabled, setsWitchIsEnabled] = useState<boolean>(false);
+  const [inputText, onChangeInputText] = useState<string>("Digite seu nome");
+
+  const toggleSwitch = () => {
+    setsWitchIsEnabled((previousState) => !previousState);
+  };
+
+  const handleGoToCards = () => {
+    navigation.navigate("Cards", { name: inputText });
+  };
+
+  return (
+    <Page
+      header={
+        <>
+          {currentTheme === "light" ? (
+            <Image source={IMAGES.banners.logoLight} />
+          ) : (
+            <Image source={IMAGES.banners.logoDark} />
+          )}
+        </>
+      }
+    >
+      <>
+        <S.ContentSwitch>
+          <Text
+            type="h1"
+            color={
+              currentTheme === "light"
+                ? theme.light.colors.basicTextColor
+                : theme.dark.colors.basicTextColor
+            }
+            style={{ paddingBottom: 6 }}
+          >
+            Dark Mode
+          </Text>
+          <Switch
+            trackColor={{
+              false: theme.light.colors.secondary,
+              true: theme.dark.colors.primary,
+            }}
+            thumbColor={
+              switchIsEnabled
+                ? theme.dark.colors.backgroundShape
+                : theme.light.colors.backgroundShape
+            }
+            ios_backgroundColor={
+              currentTheme === "light"
+                ? theme.light.colors.secondary
+                : theme.dark.colors.backgroundShape
+            }
+            onValueChange={toggleSwitch}
+            onChange={handleChangeTheme}
+            value={switchIsEnabled}
+          />
+        </S.ContentSwitch>
+
+        <S.ContentPlay>
+          <S.InputText
+            currenTheme={currentTheme}
+            onChangeText={(text: string) => onChangeInputText(text)}
+            value={inputText}
+            clearTextOnFocus
+            maxLength={40}
+          />
+          <Button
+            width={120}
+            onPress={handleGoToCards}
+            disabled={
+              inputText === "Digite seu nome" || inputText.length < 3
+                ? true
+                : false
+            }
+          >
+            <S.ContentButton>
+              <Text type="h1" color={theme.light.colors.backgroundShape}>
+                Jogar
+              </Text>
+              <Ionicons
+                name="play"
+                size={24}
+                color={theme.dark.colors.basicTextColor}
+              />
+            </S.ContentButton>
+          </Button>
+        </S.ContentPlay>
+      </>
+    </Page>
+  );
+};
+export default Home;
