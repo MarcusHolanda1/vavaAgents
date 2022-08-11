f4b5d3087e3b127c51a3624ecd70ce811f32ba9a
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-08-11 13:50:49 -0300

    style: styling card footer in cards screen

---
+++ src/UI/screens/Cards/components/CardFooter/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+import { spacing } from "../../../../shared/theme";
+
+export const ContentButtons = styled.View`
+  padding-top: ${spacing.n12}px;
+  justify-content: space-around;
+  flex-direction: row;
+  align-items: center;
+`;
