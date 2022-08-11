a1f22b142791b962732bf8aa03d07ecb93ddbd5b
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-08-11 13:49:01 -0300

    feat: making default exports to screen cards components

---
+++ src/UI/screens/Cards/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as CardHeader } from "./CardHeader";
+export { default as CardFooter } from "./CardFooter";
+export { default as CardItem } from "./CardItem";
+export { default as ErrorBoundary } from "./ErrorBoundary";
